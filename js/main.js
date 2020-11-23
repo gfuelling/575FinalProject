88f5c1fc6e6a0e273638a6b5a1da1329900e1560
gfuelling <64756522+gfuelling@users.noreply.github.com>
2020-11-22 17:39:25 -0800

    Trying to add topojson to map
    The commented out code does not work, tried to follow example for topojson and not working yet.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,65 @@
 //javascript
+function loadPage(){
+    createMap('map1')
+    createMap('map2')
+    createMap('map3')
+}
+function createMap(div){
+    var map = L.map(div).setView([43.3, -85.76], 7);
+    
+    L.tileLayer(
+        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+        }).addTo(map);
+    //getData(map)
+}
+
+//function getData(map){
+//    L.Topojson = L.GeoJSON.extend({
+//        addData: function(data){
+//            var geojson, key;
+//            if(data.type === "Topology"){
+//                for (key in data.objects){
+//                    if (data.objects.hasOwnProperty(key)){
+//                        geojson = topojson.feature(data, data.objects[key]);
+//                        L.GeoJSON.prototype.addData.call(this, geojson)
+//                    }
+//                }
+//                return this;
+//            }
+//            L.geoJSON.prototype.addData.call(this, data);
+//            return this;
+//        }
+//    });
+//    L.Topojson = function(data,options){
+//        return new L.Topojson(data, options);
+//    };
+//    
+//    var geojson = L.Topojson(null,{
+//        style: function(feature){
+//            return{
+//                color: "#000",
+//                opacity: 1,
+//                weight: 1,
+//                fillColor: '#35495d',
+//                fillOpacity: 0.8
+//            }
+//        },
+//        onEachFeature: function(feature, layer){
+//            layer.bindPopup("test popup")
+//        }
+//        
+//    }).addTo(map);
+//    
+//    
+//    function getGeoData(url){
+//        response = fetch(url)
+//        data = response.json();
+//        console.log(data)
+//        return data
+//    }
+//    
+//    getGeoData('data/miTracts')
+//}
+
+$(document).ready(loadPage)
